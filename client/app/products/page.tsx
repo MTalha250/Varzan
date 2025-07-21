f28fa9e9fbd55a000c5274e790cebc6e4a68ac16
MTalha250 <talhabinay@gmail.com>
2025-07-21 05:31:12 +0500

    update

--- client/app/products/page.tsx
+++ client/app/products/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Categories from "@/components/products/Categories";
 import Grid from "@/components/products/Grid";
 
@@ -9,7 +9,19 @@ const Products = () => {
         <span className="text-primary font-embassy text-7xl">Our</span>
         <br /> PRODUCTS
       </h1>
-      <Categories />
+      <Suspense
+        fallback={
+          <div className="px-4 sm:px-8 md:px-16 lg:px-24 xl:px-32 py-10">
+            <div className="flex gap-8 overflow-x-auto scrollbar-hide">
+              <div className="border text-primary rounded-full border-primary py-1 px-8 animate-pulse bg-gray-200">
+                <h2 className="font-engravers text-xl">All</h2>
+              </div>
+            </div>
+          </div>
+        }
+      >
+        <Categories />
+      </Suspense>
       <div className="flex justify-center">
         <img src="/images/flower.png" className="w-32" />
       </div>
