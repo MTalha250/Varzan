bd1c9bfce15e6838bc33405c45546cbae8912727
MTalha250 <talhabinay@gmail.com>
2025-08-10 01:38:15 +0500

    update

--- client/components/network/List.tsx
+++ client/components/network/List.tsx
@@ -7,11 +7,11 @@ const List = () => {
         <img src="/images/flower.png" className="w-32" />
       </div>
       <div className="flex -translate-y-2">
-        <div className="w-1/2 pt-4 px-8 md:px-20 border-r-2 border-primary">
+        <div className="w-1/2 pt-4  border-r-2 border-primary">
           <h2 className=" text-lg sm:text-xl md:text-2xl underline underline-offset-4 text-center uppercase">
             Local
           </h2>
-          <ul className="list-disc mt-8 text-base sm:text-lg tracking-widest space-y-4">
+          <ul className="list-disc mt-8 text-base sm:text-lg tracking-widest space-y-4 pl-8 md:pl-20">
             <li>Karachi</li>
             <li>Lahore</li>
             <li>Islamabad</li>
@@ -31,11 +31,11 @@ const List = () => {
             <li>Hasilpur</li>
           </ul>
         </div>
-        <div className="w-1/2 pt-4 px-8 md:px-20 border-l-2 border-primary">
+        <div className="w-1/2 pt-4 border-l-2 border-primary">
           <h2 className="text-lg sm:text-xl md:text-2xl underline underline-offset-4 text-center uppercase">
             International
           </h2>
-          <ul className="list-disc mt-8 text-base sm:text-lg tracking-widest space-y-4">
+          <ul className="list-disc mt-8 text-base sm:text-lg tracking-widest space-y-4 pl-8 md:pl-20">
             <li>UK</li>
             <li>Saudia Arabia</li>
             <li>USA</li>
