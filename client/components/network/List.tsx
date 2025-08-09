ba5c7feba448dafb65fb87184289fe84a1ecc92f
MTalha250 <talhabinay@gmail.com>
2025-08-10 01:29:46 +0500

    update

--- client/components/network/List.tsx
+++ client/components/network/List.tsx
@@ -7,7 +7,7 @@ const List = () => {
         <img src="/images/flower.png" className="w-32" />
       </div>
       <div className="flex -translate-y-2">
-        <div className="w-1/2 pt-4 px-4 md:px-20 border-r-2 border-primary">
+        <div className="w-1/2 pt-4 px-8 md:px-20 border-r-2 border-primary">
           <h2 className=" text-lg sm:text-xl md:text-2xl underline underline-offset-4 text-center uppercase">
             Local
           </h2>
@@ -31,7 +31,7 @@ const List = () => {
             <li>Hasilpur</li>
           </ul>
         </div>
-        <div className="w-1/2 pt-4 px-4 md:px-20 border-l-2 border-primary">
+        <div className="w-1/2 pt-4 px-8 md:px-20 border-l-2 border-primary">
           <h2 className="text-lg sm:text-xl md:text-2xl underline underline-offset-4 text-center uppercase">
             International
           </h2>
